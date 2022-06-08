e587a33ead8afd843de1cca3fa7e91297f6c6119
AndrewLester <alester220@gmail.com>
2022-06-08 09:58:43 -0400

    Support vercel deployment with static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,10 @@ const config = {
     ],
 
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            pages: 'public',
+            assets: 'public',
+        }),
         prerender: {
             default: true,
         },
