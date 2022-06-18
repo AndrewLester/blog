771a14afe2ca64d2b557bca06cc8d7d8b20579da
AndrewLester <alester220@gmail.com>
2022-06-18 02:17:45 -0400

    Don't wrap image links properly

--- src/lib/components/ImageLink.svelte
+++ src/lib/components/ImageLink.svelte
@@ -32,7 +32,7 @@ a {
     border-radius: 5px;
     color: inherit;
     transition: background-color var(--transition-duration) ease;
-    word-break: nowrap;
+    word-break: keep-all;
 }
 
 a svg,
